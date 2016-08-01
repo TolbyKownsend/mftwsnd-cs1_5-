3a2987e81724e5de40e2204de452d44d228de6e5
TolbyKownsend <Tolbykownsend@gmail.com>
2016-07-31 21:40:01 -0700

    Signed-off-by: TolbyKownsend <Tolbykownsend@gmail.com>

---
+++ Hmwk/Assignment1/Gladdis_5thEd_Prob13_CBSalePrice/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin_1-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_1-Windows
+CND_ARTIFACT_NAME_Debug=gladdis_5thed_prob13_cbsaleprice
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_1-Windows/gladdis_5thed_prob13_cbsaleprice
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_1-Windows/package
+CND_PACKAGE_NAME_Debug=gladdis5thedprob13cbsaleprice.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_1-Windows/package/gladdis5thedprob13cbsaleprice.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin_1-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_1-Windows
+CND_ARTIFACT_NAME_Release=gladdis_5thed_prob13_cbsaleprice
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_1-Windows/gladdis_5thed_prob13_cbsaleprice
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_1-Windows/package
+CND_PACKAGE_NAME_Release=gladdis5thedprob13cbsaleprice.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_1-Windows/package/gladdis5thedprob13cbsaleprice.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
---
+++ Hmwk/Assignment1/HelloWorld_Prob1/Template/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on June 20, 2016, 7:00 PM
+ * Purpose: Template
+ */
+
+//System Libararies
+#include <iostream> // Input/ Output libraries
+using namespace std;//Namespace of the System Libraries
+
+//User Libraries
+
+//Global Constant
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+//Declare Variables
+    int a,b,c;//3 integer Variables
+    
+//Input Data
+cout<<"Input 2 Integer -> DDDDDDDDDD DDDDDDDDDD"<<endl;//Prompt
+cin>>a>>b;//Input the 2 integers
+//Process the Data
+            c=a+b;
+    
+//Output the processed Data
+cout<<"a="<<a<<endl;
+cout<<"b="<<b<<endl;
+cout<<"c=a+b =>"<<c<<endl;
+
+cout<<"HelloWorld"<<endl;
+
+    return 0;
+}
+
